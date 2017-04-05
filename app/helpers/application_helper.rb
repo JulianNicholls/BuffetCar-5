bf9672bcd8e7c2e48ae9c3030a8cd87ab4e6d95b
Julian Nicholls <juliannicholls29@gmail.com>
2017-04-05 12:33:42 +0100

    Add Gravatar image to recipe show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def gravatar_for chef, options = { size: 80, default: 'retro' }
+    gravatar_id = Digest::MD5.hexdigest chef.email.downcase
+    size    = options[:size]
+    default = options[:default]
+
+    gravatar_url = "https://gravatar.com/avatar/#{gravatar_id}?s=#{size}&d=#{default}"
+    image_tag gravatar_url, alt: chef.name, class: 'img-circle'
+  end
 end
