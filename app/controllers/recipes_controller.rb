109c25f8e07d3850a8a254c0412c4c0c20b9a9ea
Julian Nicholls <juliannicholls29@gmail.com>
2017-04-05 15:18:36 +0100

    Create and test new recipe creation.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,7 +8,28 @@ class RecipesController < ApplicationController
   def show
   end
 
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new recipe_params
+
+    @recipe.chef = Chef.first   # TODO
+
+    if @recipe.save
+      flash[:success] = "The recipe was added successfully."
+      redirect_to @recipe
+    else
+      render :new
+    end
+  end
+
   private
+
+  def recipe_params
+    params.require(:recipe).permit :name, :description
+  end
 
   def set_recipe
     @recipe = Recipe.find params[:id]
--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -41,4 +41,40 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_match @recipe1.description, response.body
     assert_match @recipe1.chef.name, response.body
   end
+
+  test 'Should create new valid recipe' do
+    get new_recipe_path
+
+    assert_template 'recipes/new'
+
+    recipe_name = 'Chicken Korma'
+    recipe_desc = 'Stick a chicken in some coconut cream.'
+
+    assert_difference 'Recipe.count', 1 do
+      post recipes_path, params: { recipe: {
+        name:        recipe_name,
+        description: recipe_desc
+      } }
+    end
+
+    follow_redirect!
+
+    assert_match recipe_name, response.body
+    assert_match recipe_desc, response.body
+  end
+
+  test 'Should reject invalid new recipe' do
+    get new_recipe_path
+
+    assert_no_difference 'Recipe.count' do
+      post recipes_path, params: { recipe: {
+        name:        '',
+        description: ''
+      } }
+    end
+
+    assert_template 'recipes/new'
+    assert_select 'h2.panel-title'
+    assert_select 'div.panel-body'
+  end
 end
