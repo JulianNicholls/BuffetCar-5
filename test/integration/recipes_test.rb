d1dc082a84e909b89cd1695349b5a9e3cb4c1076
Julian Nicholls <juliannicholls29@gmail.com>
2017-04-05 12:16:22 +0100

    Add and test recipe show page.

--- test/integration/recipes_test.rb
+++ test/integration/recipes_test.rb
@@ -3,8 +3,10 @@ require 'test_helper'
 class RecipesTest < ActionDispatch::IntegrationTest
   def setup
     @chef = Chef.create! name: 'Julian Nicholls', email: 'julian@nowhere.com'
-    @recipe1 = @chef.recipes.build name: 'Vegetable Lasagna', description: 'Fantastic vegeable lasagna'
-    @recipe2 = @chef.recipes.build name: 'Chilli con Carne', description: 'Spicy Chilli with beef'
+    @recipe1 = @chef.recipes.build name: 'Vegetable Lasagna',
+                                   description: 'Fantastic vegetable lasagna'
+    @recipe2 = @chef.recipes.build name: 'Chilli con Carne',
+                                   description: 'Spicy Chilli with beef'
 
     @recipe1.save
     @recipe2.save
@@ -22,5 +24,21 @@ class RecipesTest < ActionDispatch::IntegrationTest
     assert_template 'recipes/index'
     assert_match @recipe1.name, response.body
     assert_match @recipe2.name, response.body
+  end
+
+  test 'Recipe names should be links to show page' do
+    get recipes_path
+
+    assert_select 'a[href=?]', recipe_path(@recipe1), text: @recipe1.name
+    assert_select 'a[href=?]', recipe_path(@recipe2), text: @recipe2.name
+  end
+
+  test 'show page should have necessary information' do
+    get recipe_path(@recipe1)
+    assert_template 'recipes/show'
+
+    assert_match @recipe1.name, response.body
+    assert_match @recipe1.description, response.body
+    assert_match @recipe1.chef.name, response.body
   end
 end
