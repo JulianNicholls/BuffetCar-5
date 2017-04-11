cbb0e50efd1403c142286cdaba6de9cc43f1ca45
Julian Nicholls <juliannicholls29@gmail.com>
2017-04-11 14:41:54 +0100

    Log in user after signup

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -13,6 +13,7 @@ class ChefsController < ApplicationController
     @chef = Chef.new chef_params
 
     if @chef.save
+      session[:chef_id] = chef.id
       flash[:success] = "Welcome #{@chef.name}. You have signed up successfully"
       redirect_to @chef
     else
