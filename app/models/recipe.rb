dd78e7c839825ee59668f10d65a49ae79c6ec3ee
Julian Nicholls <juliannicholls29@gmail.com>
2017-04-11 11:39:41 +0100

    Recipes are returned in reverse order of creation by default

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 class Recipe < ApplicationRecord
-
   belongs_to :chef
+
+  default_scope -> { order(updated_at: :desc)}
 
   validates :name, presence: true, length: {  in: 6..50 }
   validates :description, presence: true, length: {  minimum: 12 }
