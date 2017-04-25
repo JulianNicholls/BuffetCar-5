c33fe3f16c538e41cc987ce1513d3e84c63cd8c5
Julian Nicholls <juliannicholls29@gmail.com>
2017-04-25 13:02:53 +0100

    Ingredient sort order is now name

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -2,6 +2,8 @@ class Ingredient < ApplicationRecord
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
 
+  default_scope -> { order :name }
+
   validates :name,
             presence: true,
             length: { in: 3..25 },
