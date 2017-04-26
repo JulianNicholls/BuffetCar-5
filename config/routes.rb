f8e3850da8f8e0ec77d99e32ec012367217e4358
Julian Nicholls <juliannicholls29@gmail.com>
2017-04-26 13:00:13 +0100

    Add route, model, and controllers for chatroom and messages

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
   post '/login',    to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  get '/chat',      to: 'chatrooms#show'
+
   root 'pages#home'
 
   mount ActionCable.server => '/cable'
